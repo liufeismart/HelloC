41d3417225fa93d0a56fedff5cd7eb0dce323b2c
liufeismart <liufeismart@163.com>
2018-06-07 22:16:06 +0800

    first commit:

---
+++ HelloC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloC
+//
+//  Created by liufeismart on 2018/6/7.
+//  Copyright © 2018年 liufeismart. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
